7f958f38ca11e637723707a91470a23b418c21f0
Connor Christopherson <inboxconnor@gmail.com>
2018-10-20 15:36:09 -0600

    added exploit forgiving agent

--- utility.py
+++ utility.py
@@ -10,17 +10,18 @@ class Utility:
 	# row_player : string from this list --> ['all_a', 'all_b', 'tft', 'not_tft']
 	# col_player : string from this list --> ['all_a', 'all_b', 'tft', 'not_tft']
 	def of(self, row_player, col_player):
-		pairings = [self._all_a_all_a, self._all_a_all_b, self._all_a_tft, self._all_a_not_tft,
-					self._all_b_all_a, self._all_b_all_b, self._all_b_tft, self._all_b_not_tft,
-					self._tft_all_a, self._tft_all_b, self._tft_tft, self._tft_not_tft,
-					self._not_tft_all_a, self._not_tft_all_b, self._not_tft_tft, self._not_tft_not_tft]
+		pairings = [self._all_a_all_a, self._all_a_all_b, self._all_a_tft, self._all_a_not_tft, self._all_a_exploit,
+					self._all_b_all_a, self._all_b_all_b, self._all_b_tft, self._all_b_not_tft, self._all_b_exploit,
+					self._tft_all_a, self._tft_all_b, self._tft_tft, self._tft_not_tft, self._tft_exploit,
+					self._not_tft_all_a, self._not_tft_all_b, self._not_tft_tft, self._not_tft_not_tft, self._not_tft_exploit,
+					self._exploit_all_a, self._exploit_all_b, self._exploit_tft, self._exploit_not_tft, self._exploit_exploit]
 
-		players = ['all_a', 'all_b', 'tft', 'not_tft']
+		players = ['all_a', 'all_b', 'tft', 'not_tft', 'exploit']
 
 		assert row_player in players
 		assert col_player in players
 
-		return pairings[players.index(row_player) * 4 + players.index(col_player)]()
+		return pairings[players.index(row_player) * 5 + players.index(col_player)]()
 
 	def _all_a_all_a(self):
 		return self.r_payoff[0][0] / (1. - self.discount)
@@ -34,6 +35,9 @@ class Utility:
 	def _all_a_not_tft(self):
 		return self.r_payoff[0][1] / (1. - self.discount)
 
+	def _all_a_exploit(self):
+		return self.r_payoff[0][1] / (1. - self.discount)
+
 	def _all_b_all_a(self):
 		return self.r_payoff[1][0] / (1. - self.discount)
 
@@ -45,6 +49,10 @@ class Utility:
 
 	def _all_b_not_tft(self):
 		return self.r_payoff[1][0] * self.discount / (1. - self.discount) + self.r_payoff[1][1]
+
+	def _all_b_exploit(self):
+		return self.r_payoff[1][1] * self.discount ** 2 / (1. - self.discount) + \
+			   self.r_payoff[1][1] + self.r_payoff[1][0] * self.discount
 
 	def _tft_all_a(self):
 		return self.r_payoff[0][0] / (1. - self.discount)
@@ -61,6 +69,11 @@ class Utility:
 			   self.r_payoff[1][0] * self.discount ** 2 / (1. - self.discount ** 4) + \
 			   self.r_payoff[0][0] * self.discount ** 3 / (1. - self.discount ** 4)
 
+	def _tft_exploit(self):
+		return self.r_payoff[0][1] / (1. - self.discount ** 3) + \
+			   self.r_payoff[1][1] * self.discount / (1. - self.discount ** 3) + \
+			   self.r_payoff[1][0] * self.discount ** 2 / (1. - self.discount ** 3)
+
 	def _not_tft_all_a(self):
 		return self.r_payoff[1][0] / (1. - self.discount)
 
@@ -76,5 +89,31 @@ class Utility:
 	def _not_tft_not_tft(self):
 		return self.r_payoff[1][1] / (1. - self.discount ** 2) + \
 			   self.r_payoff[0][0] * self.discount / (1. - self.discount ** 2)
+
+	def _not_tft_exploit(self):
+		return self.r_payoff[0][1] * self.discount ** 3 / (1. - self.discount) + \
+			   self.r_payoff[1][1] + self.r_payoff[0][0] * self.discount + \
+			   self.r_payoff[1][1] * self.discount **2
+
+	def _exploit_all_a(self):
+		return self.r_payoff[1][0] / (1. - self.discount)
+
+	def _exploit_all_b(self):
+		return self.r_payoff[1][1] * self.discount ** 2 / (1. - self.discount) + \
+			   self.r_payoff[1][1] + self.r_payoff[0][1] * self.discount
+
+	def _exploit_tft(self):
+		return self.r_payoff[1][0] / (1. - self.discount ** 3) + \
+			   self.r_payoff[1][1] * self.discount / (1. - self.discount ** 3) + \
+			   self.r_payoff[0][1] * self.discount ** 2 / (1. - self.discount ** 3)
+
+	def _exploit_not_tft(self):
+		return self.r_payoff[1][0] * self.discount ** 3 / (1. - self.discount) + \
+			   self.r_payoff[1][1] + self.r_payoff[0][0] * self.discount + \
+			   self.r_payoff[1][1] * self.discount **2
+
+	def _exploit_exploit(self):
+		return self.r_payoff[1][1] * self.discount ** 2 / (1. - self.discount) + \
+			   self.r_payoff[1][1] + self.r_payoff[0][0] * self.discount
 
 
