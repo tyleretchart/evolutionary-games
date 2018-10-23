4ff240d1ce5ef0d1809a57702306899dd9075421
William Myers <mwilliammyers@gmail.com>
2018-10-23 12:32:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from population import Population
 from utility import Utility
+import grapher
 
 # cardinals
 T = 5
@@ -8,7 +9,7 @@ P = 2
 S = 1
 
 # players
-PLAYERS = ['all_a', 'all_b', 'tft', 'not_tft', 'exploit']
+PLAYERS = ['all_a', 'all_b', 'tft', 'not_tft']
 
 # games
 PRISONERS_DILEMMA = [[R, S], [T, P]]
@@ -18,16 +19,16 @@ BATTLE_OF_THE_SEXES = [[0, 2], [1, 0]]
 # discount
 DISCOUNT = .95
 
-u = Utility(STAG_HUNT, DISCOUNT, PLAYERS)
+u = Utility(PRISONERS_DILEMMA, DISCOUNT, PLAYERS)
 
-initial_player_percentages = [0, .8, .2, 0, 0]
+initial_player_percentages = [.25, .25, .25, .25]
 
 pop = Population(
     players=PLAYERS,
     player_percentages=initial_player_percentages,
     u=u,
-    row_size=5,
-    col_size=5)
+    row_size=30,
+    col_size=30)
 
 # r = pop.random_utility_lattice()
 # print(r)
@@ -36,14 +37,12 @@ pop = Population(
 # print(r)
 
 results = {k: [] for k in PLAYERS}
-for i in range(10):
-    pop.step_with_imitator_dynamics()
+for i in range(50):
     for player, percent in pop.player_percentages.items():
         results[player].append(percent)
+    pop.step_with_imitator_dynamics()
 
-from pprint import pprint
-pprint(results)
-
+grapher.graph_percentages(results, "test")
 # new_pop = pop.replicator_dynamics(pop.random_utility_lattice(),
 #                                   initial_player_percentages)
 # print(pop.population)
