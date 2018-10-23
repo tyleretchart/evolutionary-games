a48b7ce248378d62b1cd11a9fb103edf76e51824
William Myers <mwilliammyers@gmail.com>
2018-10-23 15:27:27 -0600

    Add dynamic stopping

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
+import collections
+
+import grapher
 from population import Population
 from utility import Utility
-import grapher
-
-# cardinals
-T = 5
-R = 3
-P = 2
-S = 1
 
 # players
 PLAYERS = ['all_a', 'all_b', 'tft', 'not_tft']
 
 # games
-PRISONERS_DILEMMA = [[R, S], [T, P]]
+PRISONERS_DILEMMA = [[3, 1], [5, 2]]
 STAG_HUNT = [[4, 3], [0, 3]]
 BATTLE_OF_THE_SEXES = [[0, 2], [1, 0]]
 
-# discount
 DISCOUNT = .95
+DISCOUNT = .97
+# DISCOUNT = .99
 
 u = Utility(PRISONERS_DILEMMA, DISCOUNT, PLAYERS)
 
@@ -27,24 +24,26 @@ pop = Population(
     players=PLAYERS,
     player_percentages=initial_player_percentages,
     u=u,
-    row_size=30,
-    col_size=30)
-
-# r = pop.random_utility_lattice()
-# print(r)
-
-# r = pop.neighborhood_utility_lattice()
-# print(r)
+    row_size=300,
+    col_size=300)
 
 results = {k: [] for k in PLAYERS}
-for i in range(50):
+maxlen = 10
+histories = {k: collections.deque(range(maxlen), maxlen) for k in PLAYERS}
+while True:
     for player, percent in pop.player_percentages.items():
         results[player].append(percent)
-    pop.step_with_imitator_dynamics()
+        histories[player].append(percent)
 
-grapher.graph_percentages(results, "test")
-# new_pop = pop.replicator_dynamics(pop.random_utility_lattice(),
-#                                   initial_player_percentages)
-# print(pop.population)
-# print("===========================")
-# print(new_pop)
+    should_continue = False
+    for history in histories.values():
+        if len(set(round(h, 6) for h in history)) > 1:
+            should_continue = True
+            break
+
+    if not should_continue:
+        break
+
+    pop.step_with_replicator_dynamics(lr=.001)
+
+grapher.graph_percentages(results)
