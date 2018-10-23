6c2de19cee833d044794e0d448325717d98bef69
Connor Christopherson <inboxconnor@gmail.com>
2018-10-23 16:31:40 -0600

    grapher now takes lists of dicts of lists

--- grapher.py
+++ grapher.py
@@ -1,27 +1,81 @@
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
+import numpy as np
 
+def graph_percentages(percentages_list_of_dicts, title=None):
+    average_dict = combine_dicts(percentages_list_of_dicts, np.average)
+    std_dict = combine_dicts(percentages_list_of_dicts, np.std)
 
-def graph_percentages(percentages_dict, title=None):
     fig, ax = plt.subplots()
-    for key in percentages_dict:
-        ax.plot(percentages_dict[key])
+    for key in average_dict:
+        plotable = np.array(average_dict[key])
+        ax.plot(plotable)
+        ax.fill_between(np.linspace(0, plotable.shape[0]-1, plotable.shape[0]),
+                        plotable + np.array(std_dict[key]), plotable - np.array(std_dict[key]),
+                        alpha=0.5)
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
     if title:
         plt.title(title)
     plt.xlabel('Iterations')
     plt.ylabel('Population Percentage')
-    plt.legend(percentages_dict)
+    plt.legend(average_dict)
+
     plt.show()
 
+def combine_dicts(list_of_dicts, f):
+    result = {}
+    iterations = len(list_of_dicts[0][next(iter(list_of_dicts[0]))])
+    for key in list_of_dicts[0]:
+        result[key] = []
+        for i in range(iterations):
+            values = []
+            for j in range(len(list_of_dicts)):
+                if len(list_of_dicts[j][key]) > i:
+                    values.append(list_of_dicts[j][key][i])
+            result[key].append(f(values))
+    return result
 
-"""
-test = {
+
+
+test = [{
+  'all_a': [0.1, 0.15, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.3],
+  'all_b': [0.15, 0.15, 0.15, 0.5, 0.5, 0.5, 0.5, 1.0, 0.5, 0.5, 0.3, 0.3],
+  'exploit': [0.1, 0.1, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.3],
+  'not_tft': [0.1, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.3],
+  'tft': [0.58, 0.58, 0.7, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.3, 0.3]
+},
+{
+  'all_a': [0.1, 0.15, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3],
+  'all_b': [0.16, 0.16, 0.16, 0.6, 0.6, 0.6, 0.6, 1.0, 0.6, 0.6, 3.0],
+  'exploit': [0.1, 0.1, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 3.0],
+  'not_tft': [0.1, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 3.0],
+  'tft': [0.58, 0.58, 0.7, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 3.0]
+},{
+  'all_a': [0.1, 0.15, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+  'all_b': [0.14, 0.14, 0.14, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4],
+  'exploit': [0.1, 0.2, 0.15, 0.1, 0.15, 0.0, 0.0, 0.0, 0.0, 0.0],
+  'not_tft': [0.1, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+  'tft': [0.58, 0.58, 0.7, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+},{
   'all_a': [0.1, 0.15, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
   'all_b': [0.12, 0.12, 0.15, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
   'exploit': [0.1, 0.1, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
   'not_tft': [0.1, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
   'tft': [0.58, 0.58, 0.7, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-}
+},{
+  'all_a': [0.1, 0.15, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+  'all_b': [0.14, 0.14, 0.14, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4],
+  'exploit': [0.1, 0.1, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+  'not_tft': [0.1, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+  'tft': [0.58, 0.58, 0.7, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+}]
+test = [{
+  'all_a': [0.1, 0.15, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+  'all_b': [0.14, 0.14, 0.14, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4],
+  'exploit': [0.1, 0.1, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+  'not_tft': [0.1, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+  'tft': [0.58, 0.58, 0.7, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+}]
+# graph_imitator_percentages(test)
+
 graph_percentages(test)
-"""
